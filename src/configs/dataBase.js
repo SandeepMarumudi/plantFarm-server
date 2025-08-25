0193677d562f92360af80906ef612b1b982c399c
SandeepMarumudi <marumudisandeep@gmail.com>
2025-08-25 11:38:58 +0530

    db

--- src/configs/dataBase.js
+++ src/configs/dataBase.js
@@ -1,9 +1,15 @@
-const mongoose=require("mongoose")
-const connectDB=async()=>{
-    try{
-        await mongoose.connect("mongodb+srv://sandy2242:2KOLKM9CvRfjxkZD@namstenode.vefwfiq.mongodb.net/plantFarm")
-    }catch(err){
-        console.log(err)
-    }
-}
-module.exports=connectDB
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sandy2242:2KOLKM9CvRfjxkZD@namstenode.vefwfiq.mongodb.net/plantFarm",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = connectDB;
