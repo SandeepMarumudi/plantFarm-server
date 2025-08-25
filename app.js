99ca6b944c035ff0ba502a1176704515b8739aa0
SandeepMarumudi <marumudisandeep@gmail.com>
2025-08-25 11:55:09 +0530

    ss

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin:"https://plantfarm-server.onrender.com",
+  origin:"http://localhost:5173",
   credentials:true
 })) //cors error while fectching api's from the frontEnd
 
