d98f337089c5e2a4eabd630dab933acf397a03e3
SandeepMarumudi <marumudisandeep@gmail.com>
2025-08-25 11:42:43 +0530

    cors

--- src/configs/dataBase.js
+++ src/configs/dataBase.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://sandy2242:2KOLKM9CvRfjxkZD@namstenode.vefwfiq.mongodb.net/plantFarm",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://sandy2242:2KOLKM9CvRfjxkZD@namstenode.vefwfiq.mongodb.net/plantFarm"
     );
   } catch (err) {
     console.log(err);
